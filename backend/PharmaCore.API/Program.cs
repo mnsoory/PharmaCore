268c1de9d62fb0d727645980473da79d826cf76c
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-07 23:04:10 +0200

    feat: configure DI, Swagger, CORS, and Middlewares in Program.cs

--- backend/PharmaCore.API/Program.cs
+++ backend/PharmaCore.API/Program.cs
@@ -1,11 +1,20 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using PharmaCore.API.Middlewares;
+using PharmaCore.Application.Mappings;
+using PharmaCore.Application.Services;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Core.Interfaces.Security;
+using PharmaCore.Core.Interfaces.Services;
 using PharmaCore.Infrastructure.Data;
+using PharmaCore.Infrastructure.Repositories;
+using PharmaCore.Infrastructure.Security;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -25,16 +34,63 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     }
 });
 
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Services
+builder.Services.AddScoped<IUserService, UserService>();
+
+// Mappings
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddMaps(typeof(UserProfile).Assembly);
+});
+
+// Security
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.UseAllOfToExtendReferenceSchemas();
+});
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
