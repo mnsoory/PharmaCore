ada086f1e22aaebdc1f0f9991d7f3e8b4c955efa
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-07 23:01:08 +0200

    feat: implement global exception handling middleware

---
+++ backend/PharmaCore.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using PharmaCore.Core.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace PharmaCore.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = exception switch
+            {
+                BusinessException => (int)HttpStatusCode.BadRequest, // 400
+                NotFoundException => (int)HttpStatusCode.NotFound,   // 404
+                _ => (int)HttpStatusCode.InternalServerError         // 500
+            };
+
+            context.Response.StatusCode = statusCode;
+
+            var response = new
+            {
+                StatusCode = statusCode,
+                Message = exception.Message,
+                Timestamp = DateTime.UtcNow
+            };
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
