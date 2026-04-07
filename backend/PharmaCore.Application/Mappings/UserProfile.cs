49df2cd5289fd47d4491337ddea10e8860758819
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-07 22:12:01 +0200

    feat: implement UserService and AutoMapper profiles for registration

---
+++ backend/PharmaCore.Application/Mappings/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿
+
+using AutoMapper;
+using PharmaCore.Core.DTOs.User;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Application.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<RegisterUserDto, User>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => false));
+        }
+    }
+}
---
+++ backend/PharmaCore.Application/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿
+
+using AutoMapper;
+using PharmaCore.Core.DTOs.User;
+using PharmaCore.Core.Entities;
+using PharmaCore.Core.Exceptions;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Core.Interfaces.Security;
+using PharmaCore.Core.Interfaces.Services;
+using System.Threading.Tasks;
+
+namespace PharmaCore.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _mapper = mapper;
+        }
+
+        public async Task<int> RegisterUser(RegisterUserDto userDto)
+        {
+            if (await _userRepository.ExistsAsync(userDto.Username))
+                throw new BusinessException($"Username '{userDto.Username}' is already taken.");
+
+            if (await _userRepository.EmailExistsAsync(userDto.Email))
+                throw new BusinessException($"Email '{userDto.Email}' is already registered.");
+
+            if (userDto.Phone is not null && await _userRepository.PhoneExistsAsync(userDto.Phone))
+                throw new BusinessException($"Phone number '{userDto.Phone}' is already in use.");
+
+            User userEntity = _mapper.Map<User>(userDto);
+            userEntity.PasswordHash = _passwordHasher.HashPassword(userDto.Password);
+
+            await _userRepository.AddAsync(userEntity);
+            return userEntity.UserId;
+        }
+    }
+}
