ad07588738728edfa0ff0a6cf6c873b4f3766c8a
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-07 22:59:25 +0200

    feat: add UsersController and configure API dependencies

---
+++ backend/PharmaCore.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using PharmaCore.Core.DTOs.User;
+using PharmaCore.Core.Interfaces.Services;
+
+namespace PharmaCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto userDto)
+        {
+            int newUserId = await _userService.RegisterUser(userDto);
+            return Created();
+        }
+    }
+}
