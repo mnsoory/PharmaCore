29b9fc64b21f0d510b21e882d20acfa8c05a5fd1
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-07 22:18:05 +0200

    feat: implement PasswordHasher using BCrypt

---
+++ backend/PharmaCore.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿
+
+using PharmaCore.Core.Interfaces.Security;
+using BC = BCrypt.Net.BCrypt;
+
+namespace PharmaCore.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password) => BC.HashPassword(password);
+
+        public bool VerifyPassword(string password, string passwordHash) => BC.Verify(password, passwordHash);
+    }
+}
