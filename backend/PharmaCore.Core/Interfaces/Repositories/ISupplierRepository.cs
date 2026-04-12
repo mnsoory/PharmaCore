4c0c6a77fdf87450fadc49ce1cd82b35c33e1bf8
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-12 15:09:58 +0200

    feat(supplier): implement ISupplierRepository and its EF Core infrastructure implementation

---
+++ backend/PharmaCore.Core/Interfaces/Repositories/ISupplierRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Core.Interfaces.Repositories
+{
+    public interface ISupplierRepository
+    {
+        Task<Supplier?> GetByIdAsync(int id);
+        Task<Supplier?> GetByIdIgnoreQueryFilterAsync(int id);
+        Task<IEnumerable<Supplier>> GetAllAsync();
+
+        Task<IEnumerable<Supplier>> GetAllInActiveAsync();
+        Task AddAsync(Supplier supplier);
+        Task UpdateAsync(Supplier supplier);
+        Task<bool> ExistsByPhoneAsync(string phone);
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Repositories/SupplierRepository.cs
@@ -0,0 +1,66 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PharmaCore.Core.Entities;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Infrastructure.Data;
+
+namespace PharmaCore.Infrastructure.Repositories
+{
+    public class SupplierRepository : ISupplierRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SupplierRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Supplier supplier)
+        {
+            await _context.Suppliers.AddAsync(supplier);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Supplier>> GetAllAsync()
+        {
+            return await _context.Suppliers
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Supplier>> GetAllInActiveAsync()
+        {
+            return await _context.Suppliers
+                .AsNoTracking()
+                .IgnoreQueryFilters()
+                .Where(s => !s.IsActive)
+                .ToListAsync();
+        }
+
+        public async Task<Supplier?> GetByIdAsync(int id)
+        {
+            return await _context.Suppliers.FindAsync(id);
+        }
+
+        public async Task<Supplier?> GetByIdIgnoreQueryFilterAsync(int id)
+        {
+            return await _context.Suppliers
+                .IgnoreQueryFilters()
+                .FirstOrDefaultAsync(s => s.SupplierId == id);
+        }
+
+        public async Task UpdateAsync(Supplier supplier)
+        {
+            _context.Update(supplier);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsByPhoneAsync(string phone)
+        {
+            return await _context.Suppliers
+                .IgnoreQueryFilters()
+                .AnyAsync(s => s.Phone == phone);
+        }
+    }
+}
