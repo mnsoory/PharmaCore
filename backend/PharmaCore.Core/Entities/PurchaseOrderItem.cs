c5d6365fef66b15a6e6d4655b84c57fe1dfe726c
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-14 10:18:37 +0200

    feat: add PurchaseOrderItem entity and configuration with cascade delete for orders

---
+++ backend/PharmaCore.Core/Entities/PurchaseOrderItem.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace PharmaCore.Core.Entities
+{
+    public class PurchaseOrderItem
+    {
+        public int PurchaseOrderItemId { get; set; }
+        public int PurchaseOrderId { get; set; }
+        public int DrugId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public virtual PurchaseOrder PurchaseOrder { get; set; } = null!;
+        public virtual Drug Drug { get; set; } = null!;
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/PurchaseOrderItemConfiguration.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class PurchaseOrderItemConfiguration : IEntityTypeConfiguration<PurchaseOrderItem>
+    {
+        public void Configure(EntityTypeBuilder<PurchaseOrderItem> builder)
+        {
+            builder.ToTable("PurchaseOrderItems");
+            builder.HasKey(poi => poi.PurchaseOrderItemId);
+
+            builder.Property(poi => poi.Quantity)
+                .IsRequired();
+
+            builder.Property(poi => poi.UnitPrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(poi => poi.PurchaseOrder)
+                .WithMany(po => po.Items)
+                .HasForeignKey(poi => poi.PurchaseOrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(poi => poi.Drug)
+                .WithMany()
+                .HasForeignKey(poi => poi.DrugId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
