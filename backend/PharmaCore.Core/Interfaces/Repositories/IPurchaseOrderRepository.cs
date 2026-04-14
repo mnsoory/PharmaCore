1cdaaadc57f0d5c09b2f99a68116ae3c96954af6
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-14 10:23:48 +0200

    feat: implement IPurchaseOrderRepository and its infrastructure for order management

---
+++ backend/PharmaCore.Core/Interfaces/Repositories/IPurchaseOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Core.Interfaces.Repositories
+{
+    public interface IPurchaseOrderRepository
+    {
+        Task<PurchaseOrder?> GetByIdAsync(int id);
+        Task<PurchaseOrder?> GetWithDetailsAsync(int id);
+        Task<IEnumerable<PurchaseOrder>> GetAllAsync();
+        Task<IEnumerable<PurchaseOrder>> GetBySupplierIdAsync(int supplierId);
+        Task AddAsync(PurchaseOrder order);
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Repositories/PurchaseOrderRepository.cs
@@ -0,0 +1,62 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PharmaCore.Core.Entities;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Infrastructure.Data;
+
+namespace PharmaCore.Infrastructure.Repositories
+{
+    public class PurchaseOrderRepository : IPurchaseOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PurchaseOrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(PurchaseOrder order)
+        {
+            await _context.PurchaseOrders.AddAsync(order);
+        }
+
+        public async Task<IEnumerable<PurchaseOrder>> GetAllAsync()
+        {
+            return await _context.PurchaseOrders
+                .AsNoTracking()
+                .Include(p => p.User)
+                .Include(p => p.Supplier)
+                .Include(p => p.Items)
+                    .ThenInclude(i => i.Drug)
+                .ToListAsync();
+        }
+
+        public async Task<PurchaseOrder?> GetByIdAsync(int id)
+        {
+            return await _context.PurchaseOrders
+                .FirstOrDefaultAsync(p => p.PurchaseOrderId ==id);
+        }
+
+        public async Task<PurchaseOrder?> GetWithDetailsAsync(int id)
+        {
+            return await _context.PurchaseOrders
+                .AsNoTracking()
+                .Include(p => p.User)
+                .Include(p => p.Supplier)
+                .Include(p => p.Items)
+                    .ThenInclude(i => i.Drug)
+                .FirstOrDefaultAsync(p => p.PurchaseOrderId == id);
+        }
+
+        public async Task<IEnumerable<PurchaseOrder>> GetBySupplierIdAsync(int supplierId)
+        {
+            return await _context.PurchaseOrders
+                .AsNoTracking()
+                .Where(p => p.SupplierId == supplierId)
+                .ToListAsync();
+        }
+
+        
+    }
+}
