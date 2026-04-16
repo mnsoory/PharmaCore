19d56284f8f470e14b8de5bb286d956c7f0a05bc
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-16 18:12:55 +0200

    feat: implement IStockAdjustmentRepository and its infrastructure implementation

---
+++ backend/PharmaCore.Core/Interfaces/Repositories/IStockAdjustmentRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Core.Interfaces.Repositories
+{
+    public interface IStockAdjustmentRepository
+    {
+        Task AddAsync(StockAdjustment adjustment);
+        Task<StockAdjustment?> GetByIdAsync(int id);
+        Task<IEnumerable<StockAdjustment>> GetByDrugIdAsync(int drugId);
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Repositories/StockAdjustmentRepository.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PharmaCore.Core.Entities;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Infrastructure.Data;
+
+namespace PharmaCore.Infrastructure.Repositories
+{
+    public class StockAdjustmentRepository : IStockAdjustmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public StockAdjustmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(StockAdjustment adjustment)
+        {
+            await _context.StockAdjustments.AddAsync(adjustment);
+        }
+
+        public async Task<StockAdjustment?> GetByIdAsync(int id)
+        {
+            return await _context.StockAdjustments
+                .Include(x => x.StockBatch)
+                    .ThenInclude(s => s.Drug)
+                .Include(x => x.User)
+                .FirstOrDefaultAsync(x => x.StockAdjustmentId == id);
+        }
+
+        public async Task<IEnumerable<StockAdjustment>> GetByDrugIdAsync(int drugId)
+        {
+            return await _context.StockAdjustments
+                .Include(x => x.StockBatch)
+                    .ThenInclude(s => s.Drug)
+                .Include(x => x.User)
+                .Where(x => x.StockBatch.DrugId == drugId)
+                .OrderByDescending(x => x.AdjustmentDate)
+                .ToListAsync();
+        }
+    }
+}
