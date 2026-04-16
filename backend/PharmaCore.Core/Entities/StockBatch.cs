4d876266eaa48c8cd4624b2573c4e351584137ab
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-16 11:59:50 +0200

    feat: add StockBatch entity and configuration with performance indexes

---
+++ backend/PharmaCore.Core/Entities/StockBatch.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace PharmaCore.Core.Entities
+{
+    public class StockBatch
+    {
+        public int StockBatchId { get; set; }
+        public int SupplierId { get; set; }
+        public int PurchaseOrderItemId { get; set; }
+        public int DrugId { get; set; }
+        public string BatchNumber { get; set; } = string.Empty;
+        public DateTime ExpiryDate { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public int Quantity { get; set; }
+        public int RemainingQty { get; set; }
+        public decimal PurchasePrice { get; set; }
+
+        public virtual Supplier Supplier { get; set; } = null!;
+        public virtual PurchaseOrderItem PurchaseOrderItem { get; set; } = null!;
+        public virtual Drug Drug { get; set; } = null!;
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/StockBatchConfiguration.cs
@@ -0,0 +1,58 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class StockBatchConfiguration : IEntityTypeConfiguration<StockBatch>
+    {
+        public void Configure(EntityTypeBuilder<StockBatch> builder)
+        {
+            builder.ToTable("StockBatches");
+            builder.HasKey(s => s.StockBatchId);
+
+            builder.Property(s => s.BatchNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.ExpiryDate)
+                .IsRequired();
+
+            builder.Property(s => s.ProductionDate)
+                .IsRequired();
+
+            builder.Property(s => s.Quantity)
+                .IsRequired();
+
+            builder.Property(s => s.RemainingQty)
+                .IsRequired();
+
+            builder.Property(s => s.PurchasePrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasIndex(b => new { b.DrugId, b.ExpiryDate })
+                .HasDatabaseName("IX_StockBatches_DrugId_ExpiryDate");
+
+            builder.HasIndex(b => new { b.DrugId, b.ExpiryDate, b.RemainingQty })
+                .HasDatabaseName("IX_StockBatches_DrugId_ExpiryDate_RemainingQty");
+
+            builder.HasOne(s => s.Supplier)
+                .WithMany(sup => sup.StockBatches)
+                .HasForeignKey(s => s.SupplierId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Drug)
+                .WithMany(d => d.StockBatches)
+                .HasForeignKey(s => s.DrugId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.PurchaseOrderItem)
+                .WithMany()
+                .HasForeignKey(s => s.PurchaseOrderItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
