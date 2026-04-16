9e614b79c3703531234e30cefb10e053b82e1582
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-16 18:03:11 +0200

    feat(stock): add StockAdjustment entity and configuration

---
+++ backend/PharmaCore.Core/Entities/StockAdjustment.cs
@@ -0,0 +1,21 @@
+﻿
+
+using PharmaCore.Core.Enums;
+
+namespace PharmaCore.Core.Entities
+{
+    public class StockAdjustment
+    {
+        public int StockAdjustmentId { get; set; }
+        public int StockBatchId { get; set; }
+        public int UserId { get; set; }
+
+        public int Quantity { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public StockAdjustmentType AdjustmentType { get; set; }
+        public DateTime AdjustmentDate { get; set; } = DateTime.UtcNow;
+
+        public virtual StockBatch StockBatch { get; set; } = null!;
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/StockAdjustmentConfiguration.cs
@@ -0,0 +1,47 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class StockAdjustmentConfiguration : IEntityTypeConfiguration<StockAdjustment>
+    {
+        public void Configure(EntityTypeBuilder<StockAdjustment> builder)
+        {
+            builder.ToTable("StockAdjustments");
+
+            builder.HasKey(x => x.StockAdjustmentId);
+
+            builder.Property(x => x.Quantity)
+                .IsRequired();
+
+            builder.Property(x => x.Reason)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(x => x.AdjustmentType)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasConversion<string>();
+
+            builder.Property(x => x.AdjustmentDate)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.HasIndex(x => x.StockBatchId);
+            builder.HasIndex(x => x.AdjustmentDate);
+
+            builder.HasOne(x => x.StockBatch)
+                .WithMany()
+                .HasForeignKey(x => x.StockBatchId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
