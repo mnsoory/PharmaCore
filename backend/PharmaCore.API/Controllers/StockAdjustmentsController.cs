5dd1b3a2277253864b985260e89c9a75884fe5a2
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-16 18:19:38 +0200

    feat: add StockAdjustments controller and AutoMapper mapping profile

---
+++ backend/PharmaCore.API/Controllers/StockAdjustmentsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharmaCore.Core.DTOs.StockAdjustment;
+using PharmaCore.Core.Interfaces.Services;
+using System.Security.Claims;
+
+namespace PharmaCore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StockAdjustmentsController : ControllerBase
+    {
+        private readonly IStockAdjustmentService _adjustmentService;
+
+        public StockAdjustmentsController(IStockAdjustmentService adjustmentService)
+        {
+            _adjustmentService = adjustmentService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<StockAdjustmentResponseDto>> Create(CreateStockAdjustmentDto dto)
+        {
+            var result = await _adjustmentService.CreateAdjustmentAsync(dto);
+            return Ok(result);
+        }
+
+        [HttpGet("drug/{drugId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<StockAdjustmentResponseDto>>> GetByDrug(int drugId)
+        {
+            var result = await _adjustmentService.GetAdjustmentsByDrugIdAsync(drugId);
+            return Ok(result);
+        }
+    }
+}
---
+++ backend/PharmaCore.Application/Mappings/StockAdjustmentProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PharmaCore.Core.DTOs.StockAdjustment;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Application.Mappings
+{
+    public class StockAdjustmentProfile : Profile
+    {
+        public StockAdjustmentProfile()
+        {
+            CreateMap<CreateStockAdjustmentDto, StockAdjustment>();
+
+            CreateMap<StockAdjustment, StockAdjustmentResponseDto>()
+                .ForMember(dest => dest.DrugId, opt => opt.MapFrom(src => src.StockBatch.Drug.DrugId))
+                .ForMember(dest => dest.DrugName, opt => opt.MapFrom(src => src.StockBatch.Drug.TradeName))
+                .ForMember(dest => dest.BatchNumber, opt => opt.MapFrom(src => src.StockBatch.BatchNumber))
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username))
+                .ForMember(dest => dest.AdjustmentType, opt => opt.MapFrom(src => src.AdjustmentType.ToString()));
+        }
+    }
+}
