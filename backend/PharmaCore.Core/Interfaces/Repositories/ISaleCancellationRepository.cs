9fa1eb2dcd9c3c22a3f4d234d3c9886fd5388f66
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-18 14:29:49 +0200

    feat: implement ISaleCancellationRepository and its SQL Server implementation

---
+++ backend/PharmaCore.Core/Interfaces/Repositories/ISaleCancellationRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Core.Interfaces.Repositories
+{
+    public interface ISaleCancellationRepository
+    {
+        Task AddAsync(SaleCancellation cancellation);
+        Task<SaleCancellation?> GetByIdAsync(int id);
+        Task<SaleCancellation?> GetBySaleIdAsync(int saleId);
+        Task<IEnumerable<SaleCancellation>> GetAllAsync();
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Repositories/SaleCancellationRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmaCore.Core.Entities;
+using PharmaCore.Core.Interfaces.Repositories;
+using PharmaCore.Infrastructure.Data;
+
+namespace PharmaCore.Infrastructure.Repositories
+{
+    public class SaleCancellationRepository : ISaleCancellationRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SaleCancellationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(SaleCancellation cancellation)
+        {
+            await _context.SaleCancellations.AddAsync(cancellation);
+        }
+
+        public async Task<SaleCancellation?> GetByIdAsync(int id)
+        {
+            return await _context.SaleCancellations
+                .Include(sc => sc.Sale)
+                .FirstOrDefaultAsync(sc => sc.SaleCancellationId == id);
+        }
+
+        public async Task<SaleCancellation?> GetBySaleIdAsync(int saleId)
+        {
+            return await _context.SaleCancellations
+                .Include(sc => sc.Sale)
+                .FirstOrDefaultAsync(sc => sc.SaleId == saleId);
+        }
+
+        public async Task<IEnumerable<SaleCancellation>> GetAllAsync()
+        {
+            return await _context.SaleCancellations
+                .AsNoTracking()
+                .Include(sc => sc.Sale)
+                .ToListAsync();
+        }
+    }
+}
