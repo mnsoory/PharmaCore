a00ed155f7ec78087abbdcc7df00919ff48cafc2
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-18 14:10:57 +0200

    feat: add SaleCancellation entity and its Fluent API configuration

---
+++ backend/PharmaCore.Core/Entities/SaleCancellation.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace PharmaCore.Core.Entities
+{
+    public class SaleCancellation
+    {
+        public int SaleCancellationId { get; set; }
+        public int SaleId { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public DateTime CancelledAt { get; set; }
+
+        public virtual Sale Sale { get; set; } = null!;
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/SaleCancellationConfiguration.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class SaleCancellationConfiguration : IEntityTypeConfiguration<SaleCancellation>
+    {
+        public void Configure(EntityTypeBuilder<SaleCancellation> builder)
+        {
+            builder.HasKey(sc => sc.SaleCancellationId);
+
+            builder.Property(sc => sc.Reason)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(sc => sc.CancelledAt)
+                .IsRequired()
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasIndex(sc => sc.SaleId)
+                .IsUnique();
+
+            builder.HasOne(sc => sc.Sale)
+                .WithOne()
+                .HasForeignKey<SaleCancellation>(sc => sc.SaleId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
