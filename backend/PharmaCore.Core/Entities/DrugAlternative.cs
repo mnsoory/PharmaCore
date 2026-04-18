11fe604bb2487873092d24dbfd814037e48bf54a
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-18 19:55:36 +0200

    feat: implement drug alternative entity and symmetric relationship configuration with database constraints

---
+++ backend/PharmaCore.Core/Entities/DrugAlternative.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace PharmaCore.Core.Entities
+{
+    public class DrugAlternative
+    {
+        public int DrugId { get; set; }
+        public Drug Drug { get; set; } = null!;
+
+        public int AlternativeDrugId { get; set; }
+        public Drug AlternativeDrug { get; set; } = null!;
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/DrugAlternativeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class DrugAlternativeConfiguration : IEntityTypeConfiguration<DrugAlternative>
+    {
+        public void Configure(EntityTypeBuilder<DrugAlternative> builder)
+        {
+            builder.HasKey(da => new { da.DrugId, da.AlternativeDrugId });
+
+            builder.HasOne(da => da.Drug)
+                .WithMany(d => d.Alternatives)
+                .HasForeignKey(da => da.DrugId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(da => da.AlternativeDrug)
+                .WithMany()
+                .HasForeignKey(da => da.AlternativeDrugId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(da => new { da.AlternativeDrugId, da.DrugId }).IsUnique();
+
+            builder.ToTable(t => t.HasCheckConstraint("CK_DrugAlternative_NoSelfAlternative", "[DrugId] <> [AlternativeDrugId]"));
+            builder.ToTable(t => t.HasCheckConstraint("CK_DrugAlternative_UniqueSymmetricPair", "[DrugId] < [AlternativeDrugId]"));
+        }
+    }
+}
