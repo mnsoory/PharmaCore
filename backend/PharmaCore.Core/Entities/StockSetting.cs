e799175a79973ba30dd36139361e00275d743f10
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-19 13:43:21 +0200

    feat: add StockSetting entity and configuration with one-to-one relationship to Drug

---
+++ backend/PharmaCore.Core/Entities/StockSetting.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace PharmaCore.Core.Entities
+{
+    public class StockSetting
+    {
+        public int DrugId { get; set; }
+        public int MinimumStock { get; set; }
+
+        public virtual Drug Drug { get; set; }
+    }
+}
---
+++ backend/PharmaCore.Infrastructure/Data/Configurations/StockSettingConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaCore.Core.Entities;
+
+namespace PharmaCore.Infrastructure.Data.Configurations
+{
+    public class StockSettingConfiguration : IEntityTypeConfiguration<StockSetting>
+    {
+        public void Configure(EntityTypeBuilder<StockSetting> builder)
+        {
+            builder.ToTable("StockSettings");
+            builder.HasKey(s => s.DrugId);
+
+            builder.HasOne(s => s.Drug)
+                    .WithOne(d => d.StockSetting)
+                    .HasForeignKey<StockSetting>(s => s.DrugId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(s => s.MinimumStock)
+                   .IsRequired()
+                   .HasDefaultValue(10);
+        }
+    }
+}
