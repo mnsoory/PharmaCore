2924365aedd2a0f7b597e9d23ffaaf1e217bedeb
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-23 10:30:30 +0200

    feat: configure protected routes and nested layouts in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./components/layout/MainLayout";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 // import LoginPage from "./pages/LoginPage";
 // import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-app-bg flex flex-col md:items-center md:justify-center overflow-auto p-0 md:p-8">
-       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-       </BrowserRouter>
+    <div className="min-h-screen w-full bg-slate-300 flex items-center justify-center p-4 md:p-10">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route element={<MainLayout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              {/* <Route path="/inventory" element={<Inventory />} /> */}
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
