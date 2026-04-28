e5225a7ec2b1b7a43ce455353c2d16ded7d17b62
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-28 16:20:21 +0300

    feat: integrate Sidebar into MainLayout and refine dashboard shell structure

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
-//import Sidebar from './Sidebar/Sidebar';
+import Sidebar from "./Sidebar/Sidebar";
 
 const MainLayout: React.FC = () => {
   return (
-    <div className="w-full max-w-7xl min-h-[90vh] bg-page-bg rounded-[2.5rem] shadow-2xl border border-white/50 flex relative">
-      <Header />
-      <div className="flex flex-1 overflow-hidden">
-        <aside className="w-64 h-full border-r border-slate-100 bg-white/20 backdrop-blur-sm">
-          {/* <Sidebar /> */}
-          <div className="p-4 text-slate-400 text-sm italic text-center mt-10">
-            Sidebar space
-          </div>
+    <div className="w-full max-w-344 min-h-[90vh] bg-page-bg rounded-[2.5rem] shadow-2xl border border-white/50 flex flex-col overflow-hidden relative">
+      <Header className="bg-blue-300/20" />
+      <div className="flex flex-1 min-h-0"> 
+        
+        <aside className="w-54 h-full border-r bg-blue-300/20 border-slate-100  backdrop-blur-sm shrink-0">
+          <Sidebar />
         </aside>
 
-        <main className="flex-1 overflow-y-auto p-8 bg-page-bg">
+        <main className="flex-1 overflow-y-auto p-8 bg-page-bg/50">
           <div className="max-w-5xl mx-auto">
             <Outlet />
           </div>
         </main>
+
       </div>
     </div>
   );
