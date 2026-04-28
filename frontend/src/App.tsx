a0bc6cb84196184d0ef8628cb2123b5466e0e67d
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-28 16:45:34 +0300

    feat: setup React Query provider and update dashboard routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import MainLayout from "./components/layout/MainLayout";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 // import LoginPage from "./pages/LoginPage";
 // import DashboardLayout from "./layouts/DashboardLayout";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="min-h-screen w-full bg-slate-300 flex items-center justify-center p-4 md:p-10">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route element={<MainLayout />}>
-              <Route path="/dashboard" element={<Dashboard />} />
-              {/* <Route path="/inventory" element={<Inventory />} /> */}
+            <Route element={<ProtectedRoute />}>
+              <Route element={<MainLayout />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+                {/* <Route path="/inventory" element={<Inventory />} /> */}
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
