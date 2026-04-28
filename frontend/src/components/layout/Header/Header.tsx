63c613dabf0615a06a4b2c027344a6ae8118e727
Mohamed Almansoury <mnsory2021@gmail.com>
2026-04-28 16:11:52 +0300

    refactor: allow custom className in Header and adjust welcome text alignment

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -4,11 +4,14 @@ import SearchBar from "./SearchBar";
 import UserActions from "./UserActions";
 import { useAuthStore } from "../../../store/useAuthStore";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+    className?:string;
+}
+const Header: React.FC<HeaderProps> =({className})  => {
   const user = useAuthStore((state) => state.user);
 
   return (
-    <header className="w-full h-min bg-gray-200/30 backdrop-blur-md border-b border-slate-100 px-8 py-6 rounded-t-[2.5rem]">
+    <header className={`z-50 w-full h-min backdrop-blur-md border-slate-100 px-8 py-6 rounded-t-[2.5rem] ${className}`}>
       <div className="max-w-480 mx-auto flex items-center justify-between">
         <div className="flex items-center flex-1">
           <Logo />
@@ -19,7 +22,7 @@ const Header: React.FC = () => {
       </div>
 
       <div className="mt-6">
-        <h2 className="text-3xl font-bold text-slate-900 tracking-tight">
+        <h2 className="text-3xl font-bold text-slate-900 ml-54 tracking-tight">
           Welcome {user?.fullName}!
         </h2>
       </div>
