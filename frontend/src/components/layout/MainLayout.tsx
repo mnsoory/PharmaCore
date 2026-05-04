a8130054822a656f95dbf26968f472f1db3a0803
Mohamed Almansoury <mnsory2021@gmail.com>
2026-05-04 18:53:59 +0300

    refactor(layout): transition to a full-screen responsive architecture - Replaced fixed-width container with a full-viewport layout (h-screen). - Optimized main content area with responsive margins and independent scrolling.

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -5,20 +5,15 @@ import Sidebar from "./Sidebar/Sidebar";
 
 const MainLayout: React.FC = () => {
   return (
-    <div className="w-full max-w-344 min-h-[90vh] bg-page-bg rounded-[2.5rem] shadow-2xl border border-white/50 flex flex-col overflow-hidden relative">
-      <Header className="bg-blue-300/20" />
-      <div className="flex flex-1 min-h-0"> 
-        
-        <aside className="w-54 h-full border-r bg-blue-300/20 border-slate-100  backdrop-blur-sm shrink-0">
-          <Sidebar />
-        </aside>
-
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-1 flex-col lg:ms-65 min-w-0">
+        <Header />
         <main className="flex-1 overflow-y-auto p-8 bg-page-bg/50">
           <div className="max-w-5xl mx-auto">
             <Outlet />
           </div>
         </main>
-
       </div>
     </div>
   );
